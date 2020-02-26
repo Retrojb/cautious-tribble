8563bc7bfdf179c16d442c17e20d3838f5fd12a2
Retrojb <baltescartography@gmail.com>
2020-02-26 17:32:08 -0500

    react_routing

---
+++ client/src/components/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import Header from './Header'
+const Landing = () => <h1>Landing Page</h1>
+const Profile = () => <h3>Profile Page</h3>
+const House = () => <h3>House Page</h3>
+const HouseNew = () => <h3>New House Page</h3>
+
+
+const App = () => {
+    return (
+        <div>
+           <BrowserRouter>
+                <div>
+                    <Header />
+                    <Route exact path="/"  component={ Landing }></Route>
+                    <Route path="/profile"  component={ Profile }></Route>
+                    <Route exact path="/house"  component={ House }></Route>
+                    <Route path="/house/new"  component={ HouseNew }></Route>
+                </div>
+           </BrowserRouter> 
+        </div>
+    )
+}
+
+export default App;
