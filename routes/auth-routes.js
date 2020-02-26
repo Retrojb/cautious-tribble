e3654d68dc0b9ec70177c29aee8bd7f042e03e37
Retrojb <baltescartography@gmail.com>
2020-02-25 19:10:33 -0500

    add_prod_keys

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -9,7 +9,14 @@ module.exports = (app) => {
     //send the code id and turns it to the actual user profile
     app.get('/auth/google/callback', passport.authenticate('google'));
 
+    app.get('/api/logout', (req, res) => {
+        req.logout();
+        res.send(req.user);
+    });
+
     app.get('/api/current_user', (req, res) => {
         res.send(req.user);
-    })
+    });
+
+
 }
