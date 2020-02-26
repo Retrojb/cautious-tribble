fcbcd00842f9d955866505a1125caf7e2faa9174
Retrojb <baltescartography@gmail.com>
2020-02-25 19:34:26 -0500

    prod_key_fix

--- config/prod.js
+++ config/prod.js
@@ -2,6 +2,6 @@ module.exports = {
     googleClientID: process.env.GOOGLE_CLIENT_ID,
     googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
     mongoURI: process.env.MONGO_URI,
-    cookieKey: COOKIE_KEY,
-    googleCallbackURL: GOOGLE_CALLBACK_URL
+    cookieKey: process.env.COOKIE_KEY,
+    googleCallbackURL: process.env.GOOGLE_CALLBACK_URL
 };
