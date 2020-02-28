8bc637f74c1d9f7c03a29c0d44f25846a968e648
Retrojb <baltescartography@gmail.com>
2020-02-27 19:55:17 -0500

    billing_success

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -3,8 +3,14 @@ const stripe = require('stripe')(keys.stripeSecretKey);
  
 module.exports = app => {
 
-    app.post('/api/stripe', (req, res) => {
-        console.log(req.body)
+    app.post('/api/stripe', async (req, res) => {
+        const charge = await stripe.charges.create({
+            amount:500,
+            currency: 'usd',
+            description: 'Charged ',
+            source: req.body.id
+        })
+        console.log(charge);
     });
 };
 
